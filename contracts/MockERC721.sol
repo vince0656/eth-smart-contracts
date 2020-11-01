a6fbee7aae4718a6dc50e5f6c214216b074d8205
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2020-11-01 21:46:30 +0000

    mock 721

---
+++ contracts/MockERC721.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MockERC721 is ERC721 {
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) public {}
+}
